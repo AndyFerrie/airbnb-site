447cd9c466049f10a9d0812396b3f84d6a8d25de
AndyFerrie <93442058+AndyFerrie@users.noreply.github.com>
2023-09-05 22:21:34 +0100

    Adds logout functionality and logged in menu items

--- app/layout.tsx
+++ app/layout.tsx
@@ -8,6 +8,7 @@ import ClientOnly from './components/ClientOnly';
 import RegisterModal from './components/modals/RegisterModal';
 import ToasterProvider from './providers/ToasterProvider';
 import LoginModal from './components/modals/LoginModal';
+import getCurrentUser from './actions/getCurrentUser';
 
 const inter = Inter({ subsets: ['latin'] })
 
@@ -20,11 +21,13 @@ const font = Nunito({
   subsets: ["latin"],
 });
 
-export default function RootLayout({
+export default async function RootLayout({
   children,
 }: {
   children: React.ReactNode
 }) {
+  const currentUser = await getCurrentUser();
+
   return (
     <html lang="en">
       <body className={font.className}>
@@ -32,7 +35,7 @@ export default function RootLayout({
           <ToasterProvider />
           <LoginModal />
           <RegisterModal />
-          <Navbar />
+          <Navbar currentUser={currentUser}/>
         </ClientOnly>
         {children}
       </body>
